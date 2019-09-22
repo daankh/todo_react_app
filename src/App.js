d3631c0944ddcc6480b5e67e2da98a64c73d7e27
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-22 19:56:53 +0200

    add Provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import AppRouter from './routers/AppRouter'
+import configureStore from "./store/configureStore";
 import './App.css';
+
+const store = configureStore();
+console.log(store.getState())
 
 function App() {
   return (
---
+++ src/reducers/todos.js
@@ -0,0 +1,9 @@
+
+const todosReducerDefaultState = [];
+
+export default (state = todosReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
