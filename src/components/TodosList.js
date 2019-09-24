5c6be13d379592550a1943c5da5cd47aed7a3dee
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-24 15:32:56 +0200

    use destructuring in function args

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -3,11 +3,11 @@ import { connect } from 'react-redux';
 import Box from '@material-ui/core/Box';
 import TodosListItem from './TodosListItem';
 
-const TodoList = (props) => {
+const TodoList = ({ todos }) => {
 
   return (
     <Box component="div">
-      {props.todos.map((todo) => {
+      {todos.map((todo) => {
         return <TodosListItem key={todo.id} {...todo} />
       })}
     </Box>
