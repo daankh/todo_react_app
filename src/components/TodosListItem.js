c16295443aed7388d78444df57f7fd7fa06e34c8
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-24 09:46:02 +0200

    add creation date display, add checkbox

--- src/components/TodosListItem.js
+++ src/components/TodosListItem.js
@@ -4,6 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import moment from 'moment';
 
 const useStyles = makeStyles(theme => ({
   box: {
@@ -23,23 +25,37 @@ const useStyles = makeStyles(theme => ({
   input: {
     display: 'none',
   },
+  timestamp: {
+    marginRight: theme.spacing(1),
+  },
 }));
 
 const TodosListItem = ({ id, name, timestamp, done, description, comments }) => {
   const classes = useStyles();
 
   return (
-    <Box component="div" p={1} className={classes.box}>
-      <Typography variant="h4" component="h4" className={classes.heading}>
-        {name}
-      </Typography>
-      <Button variant="contained" color="primary" className={classes.button} onClick={(e) => console.log('edit')}>
-        Edit
+    <React.Fragment>
+      <Box component="div" p={1} className={classes.box}>
+        <Checkbox
+          value="checkedA"
+          inputProps={{ 'aria-label': 'Checkbox A' }
+          }
+        />
+        <Typography variant="h4" component="h4" className={classes.heading}>
+          {name}
+        </Typography>
+        <Typography className={classes.timestamp}>
+          <span>created at: </span>
+          {moment(timestamp).format('DD/MM/YYYY')}
+        </Typography>
+        <Button variant="contained" color="primary" className={classes.button} onClick={(e) => console.log('edit')}>
+          Edit
       </Button>
-      <Button variant="contained" color="secondary" className={classes.button} onClick={(e) => console.log('remove')}>
-        Delete
+        <Button variant="contained" color="secondary" className={classes.button} onClick={(e) => console.log('remove')}>
+          Delete
       </Button>
-    </Box>
+      </Box>
+    </React.Fragment>
   );
 }
 
