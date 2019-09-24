ce0ebdf2e3bc0e96d4806eae6be471007fa5250e
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-24 10:04:22 +0200

    add show description

--- src/components/TodosListItem.js
+++ src/components/TodosListItem.js
@@ -1,21 +1,29 @@
 import React from 'react';
-// import moment from 'moment';
+import moment from 'moment';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
-import moment from 'moment';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+// import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(theme => ({
-  box: {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: '10px'
+  },
+  row: {
     display: 'flex',
     alignItems: 'center',
     width: '100%',
-    backgroundColor: '#bdc3c7',
-    marginBottom: '2px'
+    backgroundColor: '#FFF',
   },
-  heading: {
+  mainHeading: {
     flexGrow: 1,
   },
   button: {
@@ -28,20 +36,27 @@ const useStyles = makeStyles(theme => ({
   timestamp: {
     marginRight: theme.spacing(1),
   },
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
 }));
 
 const TodosListItem = ({ id, name, timestamp, done, description, comments }) => {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
-      <Box component="div" p={1} className={classes.box}>
+    <Box component="div" className={classes.wrapper}>
+      <Box component="div" p={1} className={classes.row}>
         <Checkbox
           value="checkedA"
           inputProps={{ 'aria-label': 'Checkbox A' }
           }
         />
-        <Typography variant="h4" component="h4" className={classes.heading}>
+        <Typography variant="h4" component="h4" className={classes.mainHeading}>
           {name}
         </Typography>
         <Typography className={classes.timestamp}>
@@ -55,7 +70,25 @@ const TodosListItem = ({ id, name, timestamp, done, description, comments }) => 
           Delete
       </Button>
       </Box>
-    </React.Fragment>
+      <Box component="div" className={classes.row}>
+        <div className={classes.root}>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>Show details</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                {description}
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </div>
+      </Box>
+    </Box>
   );
 }
 
