478638bb904d21d3ece3a51cc5d1790837d15d4c
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-25 12:46:25 +0200

    add input for adding todo name

--- src/components/TodoFrom.js
+++ src/components/TodoFrom.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import moment from 'moment';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import FormControl from '@material-ui/core/FormControl';
@@ -11,26 +12,41 @@ import { classes } from 'istanbul-lib-coverage';
 const useStyles = makeStyles(theme => ({
     form: {
         width: '100%',
-    },
-    input: {
-        width: '100%',
     }
 }));
 
 class TodoForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            name: '',
+            timestamp: moment().valueOf(),
+            done: false,
+            description: '',
+        }
     }
+
+    inputChangeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
+        const { name } = this.state
         return (
             <Fragment>
                 <ButtonAppBar showHomeBtn={true} />
                 <Box component="div" p={1}>
-                    <form className={classes.form}>
-                        <FormControl className={classes.input}>
+                    <form className={classes.form} >
+                        <FormControl fullWidth={true}>
                             <InputLabel htmlFor="my-input">Todo name</InputLabel>
-                            <Input type='text' id="my-input" aria-describedby="my-helper-text" />
+                            <Input type='text'
+                                name='name'
+                                id="my-input"
+                                aria-describedby="my-helper-text"
+                                value={name}
+                                onChange={this.inputChangeHandler} />
                             <FormHelperText id="my-helper-text">Type your new todo name</FormHelperText>
                         </FormControl>
                     </form>
