d1b70b5f3b45732457b58c14199c81e3345afaf9
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-27 10:54:59 +0200

    rebuild NotFoundPage

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import AppBar from './AppBar'
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <p>Page not found - 404</p>
-            <p>
-                <NavLink to="/">Go back to home page</NavLink>
-            </p>
-        </div>
+        <Fragment>
+            <AppBar />
+            <Box component='div' p={2}>
+                <Typography variant="h5" component="h5">
+                    <NavLink to="/">Page not found - go back to home page</NavLink>
+                </Typography>
+            </Box>
+        </Fragment>
     )
 }
 
