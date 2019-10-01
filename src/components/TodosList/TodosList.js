de1bc7ae54ae20834ff8b15d7ef23e8ec2ff959f
Daniel Jarosz <jarosz.dan@gmail.com>
2019-10-01 15:52:51 +0200

    complete todo list

--- src/components/TodosList/TodosList.js
+++ src/components/TodosList/TodosList.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import NoTodosInfo from '../Info/NoTodosInfo';
 import TodosListItem from './TodosListItem';
 import getMatchTodoComments from '../../redux/selectors/comments';
 import getMatchTodos from '../../redux/selectors/todos';
 
-const TodoList = ({ todos, comments, history }) => {
+const TodoList = ({ todos, comments, show, history }) => {
 
-  // const showDoneLast = (a, b) => a.done - b.done;
-  // const showDoneOnly = () => { todos = todos.filter(todo => todo.done) }
-  // const showOpenOnly = () => { todos = todos.filter(todo => !todo.done) }
-
-  // switch (filterType) {
-  //   case 'done':
-  //     showDoneOnly();
-  //     break;
-  //   case 'open':
-  //     showOpenOnly();
-  //     break;
-  //   default:
-  //     break;
-  // }
-
+  let heading = null;
   let content = null;
+  let headingSecond = null;
+  let contentSecond = null
+
   if (todos.length) {
-    content = todos.map((todo) => {
-      const todoComments = getMatchTodoComments(comments, todo.id)
-      return <TodosListItem key={todo.id} {...todo} comments={todoComments} history={history} />
-    })
+    if (show === 'open' || show === 'done') {
+      heading = show === 'open' ? 'Todos in progress...' : 'Completed todos';
+      content = todos.map((todo) => {
+        const todoComments = getMatchTodoComments(comments, todo.id)
+        return <TodosListItem key={todo.id} {...todo} comments={todoComments} history={history} />
+      })
+    } else {
+      content = todos.filter(todo => !todo.done).map((todo) => {
+        const todoComments = getMatchTodoComments(comments, todo.id)
+        return <TodosListItem key={todo.id} {...todo} comments={todoComments} history={history} />
+      })
+      contentSecond = todos.filter(todo => todo.done).map((todo) => {
+        const todoComments = getMatchTodoComments(comments, todo.id)
+        return <TodosListItem key={todo.id} {...todo} comments={todoComments} history={history} />
+      })
+
+      heading = content.length ? 'Todos in progress...' : null;
+      headingSecond = contentSecond.length ? 'Completed todos' : null;
+    }
   } else {
     content = (
       <NoTodosInfo />
@@ -37,15 +42,33 @@ const TodoList = ({ todos, comments, history }) => {
 
   return (
     <Box component="div">
+      <Typography
+        variant={'h4'}
+        component={'h4'}
+        align={'center'}
+        style={{ margin: '20px 0' }}>
+        {heading}
+      </Typography>
       {content}
+      {headingSecond && (
+        <Typography
+          variant={'h4'}
+          component={'h4'}
+          align={'center'}
+          style={{ margin: '20px 0' }}>
+          {headingSecond}
+        </Typography>
+      )}
+      {contentSecond && contentSecond}
     </Box>
   )
 }
 
 const mapStateToProps = ({ todos, comments, filters }) => {
   return {
-    todos: getMatchTodos(todos.todosList, filters.show),
+    todos: getMatchTodos(todos.todosList, filters.show).sort((a, b) => b.timestamp - a.timestamp),
     comments: comments.commentsList,
+    show: filters.show,
   }
 }
 
